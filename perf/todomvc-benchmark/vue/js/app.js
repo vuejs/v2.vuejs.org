083492bfe6a0c9109122e3524aded9cf06f5302f
Evan You <yyx990803@gmail.com>
2014-02-03 20:34:04 -0500

    update todomvc to be fully spec compliant

--- perf/todomvc-benchmark/vue/js/app.js
+++ perf/todomvc-benchmark/vue/js/app.js
@@ -1,115 +1,134 @@
-'use strict'
+/* global Vue, todoStorage */
+'use strict';
 
 var app = new Vue({
 
-    // the root element that will be compiled
-    el: '#todoapp',
+	// the root element that will be compiled
+	el: '#todoapp',
 
-    // a custom directive to wait for the DOM to be updated
-    // before focusing on the input field.
-    directives: {
-        'todo-focus': function (value) {
-            if (value) {
-                var el = this.el
-                setTimeout(function () { el.focus() }, 0)
-            }
-        }
-    },
+	// data
+	data: {
+		todos: todoStorage.fetch(),
+		newTodo: '',
+		editedTodo: null
+	},
 
-    // the `created` lifecycle hook, which will be called
-    // when the ViewModel instance is created but not yet compiled.
-    created: function () {
-        // setup filters
-        this.filters = {
-            all: function (todo) { todo.completed; return true },
-            active: function (todo) { return !todo.completed },
-            completed: function (todo) { return todo.completed }
-        }
-        this.updateFilter()
-        window.addEventListener('hashchange', function () {
-            app.updateFilter()
-        })
-        // initialize some state
-        this.newTodo = ''
-        this.editedTodo = null
-        this.remaining = this.todos.filter(this.filters.active).length
-    },
+	// a custom directive to wait for the DOM to be updated
+	// before focusing on the input field.
+	directives: {
+		'todo-focus': function (value) {
+			if (!value) {
+				return;
+			}
+			var el = this.el;
+			setTimeout(function () {
+				el.focus();
+			}, 0);
+		}
+	},
 
-    // data
-    data: {
-        todos: todoStorage.fetch(),
-    },
+	// the `created` lifecycle hook, which will be called
+	// when the ViewModel instance is created but not yet compiled.
+	created: function () {
+		// setup filters
+		this.filters = {
+			all: function (todo) {
+				// collect dependency.
+				// see http://vuejs.org/guide/computed.html#Dependency_Collection_Gotcha
+				/* jshint expr:true */
+				todo.completed;
+				return true;
+			},
+			active: function (todo) {
+				return !todo.completed;
+			},
+			completed: function (todo) {
+				return todo.completed;
+			}
+		};
+		this.updateFilter();
+		window.addEventListener('hashchange', function () {
+			app.updateFilter();
+		});
+		// init remaining count
+		this.remaining = this.todos.filter(this.filters.active).length;
+	},
 
-    // computed property
-    computed: {
-        allDone: {
-            $get: function () {
-                return this.remaining === 0
-            },
-            $set: function (value) {
-                this.todos.forEach(function (todo) {
-                    todo.completed = value
-                })
-                this.remaining = value ? 0 : this.todos.length
-                todoStorage.save()
-            }
-        }
-    },
+	// computed property
+	computed: {
+		allDone: {
+			$get: function () {
+				return this.remaining === 0;
+			},
+			$set: function (value) {
+				this.todos.forEach(function (todo) {
+					todo.completed = value;
+				});
+				this.remaining = value ? 0 : this.todos.length;
+				todoStorage.save();
+			}
+		}
+	},
 
-    // methods that implement data logic.
-    // note there's no DOM manipulation here at all!
-    methods: {
+	// methods that implement data logic.
+	// note there's no DOM manipulation here at all.
+	methods: {
 
-        updateFilter: function () {
-            var filter = location.hash.slice(2)
-            this.filter = (filter in this.filters) ? filter : 'all'
-            this.filterTodo = this.filters[this.filter]
-        },
+		updateFilter: function () {
+			var filter = location.hash.slice(2);
+			this.filter = (filter in this.filters) ? filter : 'all';
+			this.filterTodo = this.filters[this.filter];
+		},
 
-        addTodo: function () {
-            var value = this.newTodo && this.newTodo.trim()
-            if (value) {
-                this.todos.unshift({ title: value, completed: false })
-                this.newTodo = ''
-                this.remaining++
-                todoStorage.save()
-            }
-        },
+		addTodo: function () {
+			var value = this.newTodo && this.newTodo.trim();
+			if (!value) {
+				return;
+			}
+			this.todos.push({ title: value, completed: false });
+			this.newTodo = '';
+			this.remaining++;
+			todoStorage.save();
+		},
 
-        removeTodo: function (todo) {
-            this.todos.remove(todo.$data)
-            this.remaining -= todo.completed ? 0 : 1
-            todoStorage.save()
-        },
+		removeTodo: function (todo) {
+			this.todos.remove(todo.$data);
+			this.remaining -= todo.completed ? 0 : 1;
+			todoStorage.save();
+		},
 
-        toggleTodo: function (todo) {
-            this.remaining += todo.completed ? -1 : 1
-            todoStorage.save()
-        },
+		toggleTodo: function (todo) {
+			this.remaining += todo.completed ? -1 : 1;
+			todoStorage.save();
+		},
 
-        editTodo: function (todo) {
-            this.beforeEditCache = todo.title
-            this.editedTodo = todo
-        },
+		editTodo: function (todo) {
+			this.beforeEditCache = todo.title;
+			this.editedTodo = todo;
+		},
 
-        doneEdit: function (todo) {
-            if (!this.editedTodo) return
-            this.editedTodo = null
-            todo.title = todo.title.trim()
-            if (!todo.title) this.removeTodo(todo)
-            todoStorage.save()
-        },
+		doneEdit: function (todo) {
+			if (!this.editedTodo) {
+				return;
+			}
+			this.editedTodo = null;
+			todo.title = todo.title.trim();
+			if (!todo.title) {
+				this.removeTodo(todo);
+			}
+			todoStorage.save();
+		},
 
-        cancelEdit: function (todo) {
-            this.editedTodo = null
-            todo.title = this.beforeEditCache
-        },
-        
-        removeCompleted: function () {
-            this.todos.remove(function (todo) {
-                return todo.completed
-            })
-            todoStorage.save()
-        }
-    }
-})
+		cancelEdit: function (todo) {
+			this.editedTodo = null;
+			todo.title = this.beforeEditCache;
+		},
+		
+		removeCompleted: function () {
+			this.todos.remove(function (todo) {
+				return todo.completed;
+			});
+			todoStorage.save();
+		}
+	}
+});
--- source/examples/todomvc.md
+++ source/examples/todomvc.md
@@ -7,6 +7,6 @@ order: 3
 
 <iframe width="100%" height="500" src="/perf/todomvc-benchmark/vue/index.html" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
 
-> This is a spec-compliant TodoMVC implementation in under 100 lines of JavaScript (excluding comments and blank lines). It is also [one of the fastest implementations](/perf/).
+> This is a fully spec-compliant TodoMVC implementation in under 120 effective lines of JavaScript (excluding comments and blank lines). It is also [one of the fastest implementations](/perf/).
 
 > [[Source](https://github.com/yyx990803/vue/tree/master/examples/todomvc)]
