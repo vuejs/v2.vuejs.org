083685c771c0267420a7d19af8ed36bba87afba8
Evan You <yyou@google.com>
2014-09-29 19:07:28 -0400

    improve mobile experience

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -6,56 +6,67 @@
   var body = document.body
   var header = document.getElementById('header')
   var menu = document.querySelector('.sidebar')
-  var menuToggle = document.querySelector('.sidebar .toggle')
   var content = document.querySelector('.content')
+  var mobileBar = document.getElementById('mobile-bar')
 
-  if (!menu) return
-
-  menuToggle.addEventListener('click', function () {
+  var menuButton = mobileBar.querySelector('.menu-button')
+  menuButton.addEventListener('click', function () {
     menu.classList.toggle('open')
   })
 
-  // build sidebar
-  var allLinks = []
-  var currentPageAnchor = menu.querySelector('.sidebar-link.current')
-  var sectionContainer = document.createElement('ul')
-  sectionContainer.className = 'menu-sub'
-  currentPageAnchor.parentNode.appendChild(sectionContainer)
-  var h2s = content.querySelectorAll('h2')
-  if (h2s.length) {
-    each.call(h2s, function (h) {
-      sectionContainer.appendChild(makeLink(h))
-      var h3s = collectH3s(h)
-      allLinks.push(h)
-      allLinks.push.apply(allLinks, h3s)
-      if (h3s.length) {
-        sectionContainer.appendChild(makeSubLinks(h3s))
-      }
-    })
-  } else {
-    h2s = content.querySelectorAll('h3')
-    each.call(h2s, function (h) {
-      sectionContainer.appendChild(makeLink(h))
-      allLinks.push(h)
-    })
-  }
-
-  // init smooth scroll
-  smoothScroll.init({
-    speed: 400
+  body.addEventListener('click', function (e) {
+    if (e.target !== menuButton && !menu.contains(e.target)) {
+      menu.classList.remove('open')
+    }
   })
 
-  var animating = false
-  sectionContainer.addEventListener('click', function (e) {
-    e.preventDefault()
-    if (e.target.classList.contains('section-link')) {
-      setActive(e.target)
-      animating = true
-      setTimeout(function () {
-        animating = false
-      }, 400)
+  // build sidebar
+  var currentPageAnchor = menu.querySelector('.sidebar-link.current')
+  if (currentPageAnchor) {
+    var allLinks = []
+    var sectionContainer = document.createElement('ul')
+    sectionContainer.className = 'menu-sub'
+    currentPageAnchor.parentNode.appendChild(sectionContainer)
+    var h2s = content.querySelectorAll('h2')
+    if (h2s.length) {
+      each.call(h2s, function (h) {
+        sectionContainer.appendChild(makeLink(h))
+        var h3s = collectH3s(h)
+        allLinks.push(h)
+        allLinks.push.apply(allLinks, h3s)
+        if (h3s.length) {
+          sectionContainer.appendChild(makeSubLinks(h3s))
+        }
+      })
+    } else {
+      h2s = content.querySelectorAll('h3')
+      each.call(h2s, function (h) {
+        sectionContainer.appendChild(makeLink(h))
+        allLinks.push(h)
+      })
     }
-  }, true)
+
+    var animating = false
+    sectionContainer.addEventListener('click', function (e) {
+      e.preventDefault()
+      if (e.target.classList.contains('section-link')) {
+        menu.classList.remove('open')
+        setActive(e.target)
+        animating = true
+        setTimeout(function () {
+          animating = false
+        }, 400)
+      }
+    }, true)
+
+    // init smooth scroll
+    smoothScroll.init({
+      speed: 400,
+      offset: window.innerWidth > 720
+        ? 40
+        : 58
+    })
+  }
 
   // listen for scroll event to do positioning & highlights
   window.addEventListener('scroll', updateSidebar)
@@ -63,12 +74,13 @@
 
   function updateSidebar () {
     var top = doc && doc.scrollTop || body.scrollTop
-    if (top > header.offsetHeight) {
+    var headerHeight = header.offsetHeight
+    if (top > headerHeight) {
       main.className = 'fix-sidebar'
     } else {
       main.className = ''
     }
-    if (animating) return
+    if (animating || !allLinks) return
     var last
     for (var i = 0; i < allLinks.length; i++) {
       var link = allLinks[i]
