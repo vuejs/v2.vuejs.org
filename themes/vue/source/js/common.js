f67d83f0b26d7a91b6f35bda1699154efcf17e86
Evan You <yyx990803@gmail.com>
2015-07-24 16:00:52 -0400

    add swiftype

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -135,15 +135,13 @@
     }
   }
 
-  // Search
+  // Search with SwiftType
   
-  var postfix = encodeURI(' site:vuejs.org')
-  document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault()
-    var query = document.getElementById('search-query').value.trim()
-    if (query) {
-      window.location.href = 'https://www.google.com/search?q=' + query + postfix
-    }
-  })
+  (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+  (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+  e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+
+  _st('install','HgpxvBc7pUaPUWmG9sgv','2.0.0');
 
 })()
