849c88453d3f70b73694f09fc578ee08a399c4dd
Evan You <yyx990803@gmail.com>
2016-11-20 19:55:45 -0500

    fix let usage in common.js

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -58,10 +58,10 @@
     function levenshteinDistance (a, b) {
       var m = []
       if (!(a && b)) return (b || a).length
-      for (let i = 0; i <= b.length; m[i] = [i++]) {}
-      for (let j = 0; j <= a.length; m[0][j] = j++) {}
-      for (let i = 1; i <= b.length; i++) {
-        for (let j = 1; j <= a.length; j++) {
+      for (var i = 0; i <= b.length; m[i] = [i++]) {}
+      for (var j = 0; j <= a.length; m[0][j] = j++) {}
+      for (var i = 1; i <= b.length; i++) {
+        for (var j = 1; j <= a.length; j++) {
           m[i][j] = b.charAt(i - 1) === a.charAt(j - 1)
             ? m[i - 1][j - 1]
             : m[i][j] = Math.min(
