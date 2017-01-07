09698480b5ccfb3392680f3505eb1a80d954f241
丁一鸣 <315129552@qq.com>
2017-01-07 20:51:03 +0800

    add signup

--- themes/vue/source/js/vuefe.js
+++ themes/vue/source/js/vuefe.js
@@ -16,3 +16,25 @@ container.onmouseup = function(){
   console.log('vuefe.cn : ' + txt);
   }
 }
+
+
+/**
+ * vuefe.com auto signup
+ */
+var store = window.localStorage;
+
+var autoSignUp = function() {
+  if (!store.vuefecom_ip || !store.vuefecom_tempToken) {
+    axios.post('https://vuefecom.leanapp.cn/api/user/signuptemp')
+    .then(function(res) {
+      if (res.data.code === 0) {
+        store.vuefecom_ip = res.data.data.ip;
+        store.vuefecom_tempToken = res.data.data.tempToken;
+      }
+    });
+  } else {
+    console.log('已经登录');
+  }
+}
+
+autoSignUp();
