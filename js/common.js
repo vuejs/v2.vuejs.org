d55708a9736caafefb2d47c71e8eba06ef50d6cb
Chris Fritz <chrisvfritz@gmail.com>
2017-05-04 11:45:47 -0400

    Site updated: 2017-05-04 11:45:46

--- js/common.js
+++ js/common.js
@@ -91,7 +91,7 @@
   }
 
   /**
-   * Mobile burger menu button for toggling sidebar
+   * Mobile burger menu button and gesture for toggling sidebar
    */
 
   function initMobileMenu () {
@@ -106,6 +106,27 @@
     document.body.addEventListener('click', function (e) {
       if (e.target !== menuButton && !sidebar.contains(e.target)) {
         sidebar.classList.remove('open')
+      }
+    })
+
+    // Toggle sidebar on swipe
+    var start = {}, end = {}
+
+    document.body.addEventListener('touchstart', function (e) {
+      start.x = e.changedTouches[0].clientX
+      start.y = e.changedTouches[0].clientY
+    })
+
+    document.body.addEventListener('touchend', function (e) {
+      end.y = e.changedTouches[0].clientY
+      end.x = e.changedTouches[0].clientX
+
+      var xDiff = end.x - start.x
+      var yDiff = end.y - start.y
+
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) sidebar.classList.add('open')
+        else sidebar.classList.remove('open')
       }
     })
   }
