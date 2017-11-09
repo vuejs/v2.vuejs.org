b73f12607c7fa42520617005dce57a635211adc6
GU Yiling <justice360@gmail.com>
2017-11-10 00:46:42 +0800

    Fix header link (#1263)
    * fix header link
    
    * fix cr

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -1,5 +1,4 @@
 (function () {
-
   initMobileMenu()
   if (PAGE_TYPE) {
     initVersionSelect()
@@ -26,7 +25,7 @@
       })
     }
 
-    function createSourceSearchPath(query) {
+    function createSourceSearchPath (query) {
       query = query
         .replace(/\([^\)]*?\)/g, '')
         .replace(/(Vue\.)(\w+)/g, '$1$2" OR "$2')
@@ -251,7 +250,7 @@
         }
       }
       if (last)
-      setActive(last.id, !hoveredOverSidebar)
+        setActive(last.id, !hoveredOverSidebar)
     }
 
     function makeLink (h) {
@@ -337,8 +336,17 @@
     function makeHeaderClickable (link) {
       var wrapper = link.querySelector('a')
       wrapper.setAttribute('data-scroll', '')
-      link.parentNode.insertBefore(wrapper, link)
-      wrapper.appendChild(link)
+
+      // transform DOM structure from
+      // `<h2><a></a>Header</a>` to <h2><a>Header</a></h2>`
+      // to make the link clickable
+      var nodes = link.childNodes
+      for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i]
+        if (node !== wrapper) {
+          wrapper.appendChild(node)
+        }
+      }
     }
   }
 })()
