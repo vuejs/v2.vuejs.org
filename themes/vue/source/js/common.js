60be816756276fbdafce80035ab6d939b1d04810
lizhihua <275091674@qq.com>
2018-01-10 10:03:26 +0800

    fix video

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -146,6 +146,7 @@
   function initVideoModal () {
     var modalButton = document.getElementById('modal-player')
     var videoModal = document.getElementById('video-modal')
+    if (!modalButton || !videoModal) return
     var videoWrapper = videoModal.querySelector('.video-space')
     var overlay = document.createElement('div')
         overlay.className = 'overlay'
