8dae4ab04eb0e817d459d7d8bd49d429de33eb9f
Nico Devs <nico1485@gmail.com>
2021-06-01 12:03:54 -0300

    Add VueSchool Summer Sale banner

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -3,6 +3,7 @@
   initMobileMenu()
   initVideoModal()
   initNewNavLinks()
+  initVueSchoolBanner()
   if (PAGE_TYPE) {
     initVersionSelect()
     initApiSpecLinks()
@@ -280,6 +281,23 @@
     })
   }
 
+  /**
+   * Banner closing
+   */
+  function initVueSchoolBanner () {
+    const banner = document.getElementById('vs')
+    if (banner && !localStorage.getItem('VS_SUMMER_BANNER_CLOSED')) {
+      banner.classList.remove('vs-hidden')
+      document.body.classList.add('has-vs-banner')
+      document.getElementById('vs-close').addEventListener('click', function (e) {
+        e.preventDefault()
+        e.stopPropagation()
+        document.getElementById('vs').remove()
+        document.body.classList.remove('has-vs-banner')
+        localStorage.setItem('VS_SUMMER_BANNER_CLOSED', 1)
+      })
+    }
+  }
 
   /**
   * Modal Video Player
@@ -423,7 +441,7 @@
           if(dataTypeAttr && dataTypeAttr.nodeValue === 'theme-product-title') {
             return 300
           }
-          return 0
+          return localStorage.getItem('VS_SUMMER_BANNER_CLOSED') ? 0 : 80
         }
       })
     }
