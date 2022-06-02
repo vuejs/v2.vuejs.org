4988f3af887bd6962ca98f8eee4c1a0cdeda0bc4
Evan You <yyx990803@gmail.com>
2022-06-02 11:46:40 +0800

    use js-yaml

--- _scripts/sync-sponsors.js
+++ _scripts/sync-sponsors.js
@@ -2,13 +2,13 @@
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
-const yaml = require('yaml')
+const yaml = require('js-yaml')
 
 const configPath = path.resolve(__dirname, '../themes/vue/_config.yml')
 
 ;(async () => {
   const { data } = await axios(`https://sponsors.vuejs.org/data.json`)
-  const yml = yaml.stringify(data)
+  const yml = yaml.dump(data)
   const config = fs.readFileSync(configPath, 'utf-8')
   const updated = config.replace(
     /(# START SPONSORS)[^]*(# END SPONSORS)/,
